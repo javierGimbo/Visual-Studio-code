c57cd02e7217011d65dd099eda5d5798b3b7020c
Javier <javiergimbo96@gmail.com>
2023-10-06 12:41:14 +0200

    sdfgdfgdgdgd

--- Python/ejercicio5.py
+++ Python/ejercicio5.py
@@ -2,7 +2,7 @@ grado = int(input("Dame un grado Fahrenheit: "))
 
 celsius = (grado-32)*5/9
 
-print("La conversion de grados Fahrenheit a grados Celsius es ", celsius)
+print("La conversion de grados Fahrenheit a grados aslfhsd es ", celsius)
 
 
 #C = (F-32)*5/9
