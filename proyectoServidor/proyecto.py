04cb46ab6ade3e99fe4da7840a95d9e63e197c78
Javier <javiergimbo96@gmail.com>
2023-10-11 10:31:31 +0200

    divisiones y conferencias ok

--- proyectoServidor/proyecto.py
+++ proyectoServidor/proyecto.py
@@ -14,13 +14,13 @@ if response.status_code == 200:
     data2 = data.get("data")
 
     # Pide al usuario el nombre de un equipo
-    # Suponemos que el usuario siempre va a poner la primera letra en mayuscula por ejemplo: "Lakers"
-    nombre_equipo = input("Ingresa el nombre del equipo: ")
-
+    # .capitalize() es para que ponga siempre la primera letra en mayuscula
+    nombre_equipo = input("Ingresa el nombre del equipo: ").capitalize()
+    
     # Busca el equipo con el nombre proporcionado en los datos JSON
     indice=0
     encontrado=False
-
+    
     while indice<len(data2) and encontrado!=True:
 
         if data2[indice].get("name") == nombre_equipo:
@@ -30,24 +30,39 @@ if response.status_code == 200:
             print("Conferencia:", data2[indice]["conference"])
             print("División:", data2[indice]["division"])
             encontrado=True
-            #break
         else:
-        # Si el equipo no existe pone este mensaje
+            indice+=1
+    # Si el equipo no existe pone este mensaje
+    if not encontrado:
+         print("No se encontró un equipo con ese nombre.")
+
+    # Pregunto de que conferencia quiere saber los equipos     
+    conferenciaEquipos = input("¿De que conferencia quieres saber los equipos? WEST/EAST: ").capitalize()
+    print("EQUIPOS DE LA CONFERENCIA " + conferenciaEquipos)
+    indice=0
+    encontrado=False
+    while indice<len(data2) and encontrado!=True:
+  
+        if data2[indice].get("conference") == conferenciaEquipos:
+            print("Nombre completo:", data2[indice]["full_name"])
+            indice+=1
            
+        else:
+            indice+=1       
+   
+    divisionEquipos = input("¿De que división quieres saber los equipos?: ").capitalize()
+    print("EQUIPOS DE LA DIVISIÓN " + divisionEquipos)
+    indice=0
+    encontrado=False
+    while indice<len(data2) and encontrado!=True:
+        if data2[indice].get("division") == divisionEquipos:
+            print("Nombre completo:", data2[indice]["full_name"])
+            indice+=1    
+        else:
             indice+=1
             
-    print("No se encontró un equipo con ese nombre.")
-    ''' for equipo in data["data"]:
-        if equipo["name"] == nombre_equipo:
-            print("Nombre completo:", equipo["full_name"])
-            print("Abreviatura:", equipo["abbreviation"])
-            print("Ciudad:", equipo["city"])
-            print("Conferencia:", equipo["conference"])
-            print("División:", equipo["division"])
-            #break
-    else:
-        # Si el equipo no existe pone este mensaje
-        print("No se encontró un equipo con ese nombre.")'''
+
+
 else:
         # Si no carga el archivo Json muestra el siguiente mensaje
     print("Error al obtener los datos. Código de estado:", response.status_code)
