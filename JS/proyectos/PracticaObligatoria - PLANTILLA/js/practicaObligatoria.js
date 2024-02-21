7a231361164854ebcf68738ad411dea21e682fb2
javierGimbo <javiergimbo96@gmail.com>
2024-02-21 14:34:13 +0100

    proyecto de js

--- JS/proyectos/PracticaObligatoria - PLANTILLA/js/practicaObligatoria.js
+++ JS/proyectos/PracticaObligatoria - PLANTILLA/js/practicaObligatoria.js
@@ -29,31 +29,47 @@ document.addEventListener("DOMContentLoaded", function() {
         .catch(error => console.error('Error al obtener datos de la API:', error));
 
     
-    //CRUD categorís
+    // Función para cargar las categorías
+    function cargarCategorias() {
+        fetch("https://proyectocliente-b4fc3-default-rtdb.europe-west1.firebasedatabase.app/categorias.json")
+            .then(response => response.json())
+            .then(categorias => {
+                categoriasSelect.innerHTML = ""; // Limpiar el select antes de agregar nuevas opciones
+                for (const key in categorias) {
+                    const option = document.createElement("option");
+                    option.value = key;
+                    option.textContent = categorias[key];
+                    categoriasSelect.appendChild(option);
+                }
+            })
+            .catch(error => console.error('Error al obtener datos de la API:', error));
+    }
+
+    //CRUD categorías
     const formularioNuevaCategoria = document.getElementById("frmNuevaCategoria");
     // Función para agregar una nueva categoría
     function agregarNuevaCategoria(nombreCategoria) {
         fetch("https://proyectocliente-b4fc3-default-rtdb.europe-west1.firebasedatabase.app/categorias.json", {
-        method: "POST",
-        body: JSON.stringify(nombreCategoria),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    .then(response => response.json())
-    .then(() => {
-        // Después de agregar la categoría, agregarla al select de categorías
-        const option = document.createElement("option");
-        option.textContent = nombreCategoria;
-        categoriasSelect.appendChild(option);
-        
-        // Limpia el input y agrega la categoría al select
-        document.getElementById("txtNuevaCategoria").value = "";
+            method: "POST",
+            body: JSON.stringify(nombreCategoria),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+            // Después de agregar la categoría, agregarla al select de categorías
+            const option = document.createElement("option");
+            option.textContent = nombreCategoria;
+            categoriasSelect.appendChild(option);
+            
+            // Limpia el input y agrega la categoría al select
+            document.getElementById("txtNuevaCategoria").value = "";
 
-        // Actualiza las categorías después de 1 segundo
-        setTimeout(cargarCategorias, 1000);
-    })
-    .catch(error => console.error('Error al agregar nueva categoría:', error));
+            // Actualiza las categorías después de 1 segundo
+            setTimeout(cargarCategorias, 1000);
+        })
+        .catch(error => console.error('Error al agregar nueva categoría:', error));
     }
 
     // Escuchar evento submit del formulario para nueva categoría
@@ -64,11 +80,34 @@ document.addEventListener("DOMContentLoaded", function() {
             agregarNuevaCategoria(nuevaCategoria);
             formularioNuevaCategoria.reset();
         }
-
-        
     });
 
-    // Cargar categorías al inicio
-    cargarCategorias();
-    
+    // Función para borrar una categoría
+    function borrarCategoria(nombreCategoria) {
+        fetch(`https://proyectocliente-b4fc3-default-rtdb.europe-west1.firebasedatabase.app/categorias/${nombreCategoria}.json`, {
+            method: "DELETE"
+            
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo eliminar la categoría.");
+            }
+            // Si la respuesta es exitosa, cargar las categorías nuevamente
+            cargarCategorias();
+        })
+        .catch(error => console.error('Error al borrar categoría:', error));
+    }
+
+    // Escuchar evento submit del formulario para borrar categoría
+    const formularioBorrarCategoria = document.getElementById("frmBorrarCategoria");
+    formularioBorrarCategoria.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const eliminarCategoria = document.getElementById("txtBorrarCategoria").value; // Obtener el valor del campo
+        const nombreCategoria = eliminarCategoria.trim(); // Eliminar espacios en blanco
+        if (nombreCategoria !== "") { // Verificar si el valor no está vacío
+            borrarCategoria(nombreCategoria); // Llamar a la función borrarCategoria con el nombre de la categoría
+            formularioBorrarCategoria.reset();
+        }
+    });
 });
+
