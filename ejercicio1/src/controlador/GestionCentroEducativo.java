2975036e7df08a000ee8bcc62bd03f3f7642f7a1
javierGimbo <146812089+javierGimbo@users.noreply.github.com>
2024-09-22 19:50:33 +0200

    Update GestionCentroEducativo.java

--- ejercicio1/src/controlador/GestionCentroEducativo.java
+++ ejercicio1/src/controlador/GestionCentroEducativo.java
@@ -39,5 +39,27 @@ public class GestionCentroEducativo {
         while(it.hasNext())
             System.out.println(it.next());
     }
+    
+     public void insertarNuevoAlumno(Alumno nuevoAlumno) {
+        // Insertar el alumno en la base de datos
+        gb.insertarAlumno(nuevoAlumno);
+        // Volver a cargar la lista de alumnos desde la base de datos para reflejar los cambios
+        this.listaA = gb.descargarAlumno();
+    }
+     
+     public void borrarModulosConDuracionMayorA70() {
+        // Borrar los módulos en la base de datos
+        gb.borrarModulosConDuracionMayorA70();
+        // Volver a cargar la lista de módulos desde la base de datos para reflejar los cambios
+        this.listaM = gb.descargarModulos();
+    }
+
+    // Método para actualizar un módulo dado el nombre
+    public void actualizarModuloPorNombre(String nombreModulo, int nuevaDuracion) {
+        // Actualizar el módulo en la base de datos
+        gb.actualizarModuloPorNombre(nombreModulo, nuevaDuracion);
+        // Volver a cargar la lista de módulos desde la base de datos para reflejar los cambios
+        this.listaM = gb.descargarModulos();
+    }
 
 }
